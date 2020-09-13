359bbede387d215ab6dfab332fe2aefd4958e943
dimaarion <dimaarion@gmail.com>
2020-09-13 23:21:05 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import React from "react";
 import "./style.css";
-import { Editor } from '@tinymce/tinymce-react';
+import { Editor } from "@tinymce/tinymce-react";
 
- class App extends React.Component {
-   handleEditorChange = (content = '' , editor) => {
+class App extends React.Component {
+  handleEditorChange = (content = "", editor) => {
     content;
-   }
+  };
 
-   render() {
-     return (
-       <Editor
-         initialValue=""
-         init={{
-           height: 500,
-           menubar: false,
-           plugins: [
-             'advlist autolink lists link image charmap print preview anchor',
-             'searchreplace visualblocks code fullscreen',
-             'insertdatetime media table paste code help wordcount','image'
-           ],
-           toolbar:
-             'undo redo | formatselect | bold italic backcolor | \
+  render() {
+    return (
+      <Editor
+        initialValue=""
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code help wordcount",
+            "image"
+          ],
+          toolbar:
+            "undo redo | formatselect | bold italic backcolor | \
              alignleft aligncenter alignright alignjustify | \
-             bullist numlist outdent indent | removeformat | help'
-         }}
-         onEditorChange={this.handleEditorChange}
-       />
-     );
-   }
- }
+             bullist numlist outdent indent | removeformat | help|image"
+        }}
+        onEditorChange={this.handleEditorChange}
+      />
+    );
+  }
+}
 
- export default App;
+export default App;
